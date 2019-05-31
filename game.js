d3023653f967d79ebf8398bfcb5718f708a5b38a
asabogal <asabogal.git@gmail.com>
2019-05-31 18:24:37 -0400

    initial script

---
+++ game.js
@@ -0,0 +1,12 @@
+
+let roundScore = 0
+let scores = [0, 0]
+let activePlayer = 0
+
+
+document.querySelector('.dice').style.display = 'none';
+
+const newGame = document.querySelector('btn-new')
+const rollBtn = document.querySelector('.btn-roll')
+const holdBtn = document.querySelector('.btn-hold')
+
