27fbb81902c641a8ecfe3299a44ddc96eafbb43b
asabogal <asabogal.git@gmail.com>
2019-05-31 18:41:40 -0400

    click event listener functions on new and roll btns + change id names

--- game.js
+++ game.js
@@ -6,11 +6,24 @@ let activePlayer = 0
 
 document.querySelector('.dice').style.display = 'none';
 
-const newGame = document.querySelector('btn-new')
+
+const newGame = document.querySelector('.btn-new')
 const rollBtn = document.querySelector('.btn-roll')
 const holdBtn = document.querySelector('.btn-hold')
 
+newGame.addEventListener('click', () => {
+  document.querySelector('.dice').style.display = 'none';
+  document.getElementById('score-p0').textContent = '0'
+  document.getElementById('score-p1').textContent = '0'
+  document.getElementById('current-p0').textContent = '0'
+  document.getElementById('current-p1').textContent = '0'
+})
+
 rollBtn.addEventListener('click', () => {
   let roll = Math.floor(Math.random() * 6) + 1
-  console.log(roll)
+  let dice = document.querySelector('.dice')
+
+  dice.style.display = 'block'
+  dice.src = `./images/dice${roll}.png`
 })
+
